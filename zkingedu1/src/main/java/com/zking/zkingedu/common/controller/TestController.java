9509acfc397905b2ef4712b772a074bf50f6e9f7
jdygithub <1401190429@qq.com>
2019-08-28 09:18:58 +0800

    8.28_9:17

--- zkingedu1/src/main/java/com/zking/zkingedu/common/controller/TestController.java
+++ zkingedu1/src/main/java/com/zking/zkingedu/common/controller/TestController.java
@@ -12,6 +12,7 @@ import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -22,17 +23,9 @@ public class TestController {
     private UserService userService;
     @Autowired
     private RedisUtil redisUtil;
-
-    @RequestMapping("/")
+    @RequestMapping("/add")
     public String test(HttpServletRequest request){
-        int a = 0/9;
-        System.out.println(a);
-        System.out.println("来了");
-        List<User> allUser = userService.getAllUser();
-        System.out.println(allUser.size());
-        request.setAttribute("users",allUser);
-        request.setAttribute("welcome","yadaze");
-        return "text";
+        return "jdy/addcollection";
     }
 
 
